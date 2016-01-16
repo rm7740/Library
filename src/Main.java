f43a4f7870c559799a3cc9d8a894a0e874c36d25
rm7740 <adavid8a@hotmail.com>
2016-01-16 18:46:37 -0500

    Refactored Main & Message classes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.startSession();
+    }
+
+    public void startSession() {
         Message message = new Message();
-        message.displayWelcomeMessage();
         Shelf shelf = new Shelf();
-        shelf.showBookShelf();
+        message.displayWelcomeMessage();
+        message.displayMainMenu();
+        Scanner userInput = new Scanner(System.in);
+        int optionChosen = userInput.nextInt();
+        switch (optionChosen){
+            case 1: shelf.showBookShelf();
+                    break;
+        }
     }
 }
--- src/Message.java
+++ src/Message.java
@@ -1,8 +1,16 @@
 
 public class Message {
     String welcome = "Welcome to the library application";
+    String mainMenuTitle = "Please select an option";
+    String mainMenuOption1 = "1. List books";
+
 
     public void displayWelcomeMessage(){
         System.out.println(welcome + "\n");
+    }
+
+    public void displayMainMenu(){
+        System.out.println(mainMenuTitle);
+        System.out.println(mainMenuOption1);
     }
 }
